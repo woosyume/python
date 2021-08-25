64b893a2b2330e5668d0483c360194661026b3ad
Aaron Kim <woohyeok.kim@rakuten.com>
2021-08-25 21:52:43 +0900

    Class : Usage of property ... seems like 접근제한자

--- basic/class/basic.py
+++ basic/class/basic.py
@@ -33,12 +33,30 @@ class ToyotaCar(Car):
     # pass
 
 class TeslaCar(Car):
-    def __init__(self, model, enable_auto_run=False):
+    def __init__(self, model,
+     enable_auto_run=False,
+      password='123'):
         # 같은 부분에 대해서는 부모 생성자 호출
         super().__init__(model=model)
-        self.enable_auto_run = enable_auto_run
+        self.__enable_auto_run = enable_auto_run
+        self.password = password
+        print('hmm...', self.password)
+
+    # 읽기는 가능한데 쓰기는 불가능하도록 하는 것이 프로퍼티
+    @property
+    def enable_auto_run(self):
+        return self.__enable_auto_run
+
+    # property를 쓰기 가능하도록 할 때는 이것을 활용
+    @enable_auto_run.setter
+    def enable_auto_run(self, is_enable):
+        if self.password == '456':
+            self.__enable_auto_run = is_enable
+        else:
+            raise ValueError
 
     def auto_run(self):
+        print('aaa:', self.__enable_auto_run)
         print('auto run')
 
 car = Car()
@@ -48,7 +66,14 @@ toyota_car = ToyotaCar('Lexus')
 print(toyota_car.model)
 toyota_car.run()
 print('###########')
-tesla_car = TeslaCar('Model S')
-print(tesla_car.model)
-tesla_car.run()
-tesla_car.auto_run()
+tesla_car = TeslaCar('Model S', password='456')
+tesla_car.enable_auto_run = True
+print(tesla_car.enable_auto_run)
+
+# print(tesla_car.enable_auto_run)
+# 이것을 금지하고 싶을 때 enable_auto_run -> _enable_auto_run or ___enable_auto_run
+# _ : "볼 수는 있지만 건드리지는 말았으면 좋겠어."
+# __ : "아예 못 보게 할거야. 건드릴 생각 마."
+# __enable_auto_run 인 경우에는 클래스 내부에서만 접근 가능.
+# tesla_car.enable_auto_run = True ... AttributeError: can't set attribute
+# print(tesla_car.enable_auto_run)
