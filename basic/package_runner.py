7fcf36a8ebbd37b1ca9aca81e3ba1579ed214fb5
Aaron Kim <woohyeok.kim@rakuten.com>
2021-08-25 16:33:55 +0900

    __name__ and __main__

--- basic/package_runner.py
+++ basic/package_runner.py
@@ -13,3 +13,50 @@ print(r)
 # sing
 print(human.sing())
 print(animal.cry())
+
+ranking = {
+    'A': 100,
+    'B': 85,
+    'C': 95
+}
+
+for key in ranking:
+    print(key)
+
+# ['A', 'C', 'B']
+print(sorted(ranking, key=ranking.get, reverse=True))
+
+# 표준 라이브러리는 임포트를 해주어야 한다. 빌트인 함수는 임포트 없이 사용 가능 (e.g. print())
+s = "woosyume"
+from collections import defaultdict
+d = defaultdict(int)
+
+for c in s:
+    d[c] += 1
+
+# defaultdict(<class 'int'>, {'w': 1, 'o': 2, 's': 1, 'y': 1, 'u': 1, 'm': 1, 'e': 1})
+# 몇 개 있는지 확인
+print(d)
+print(d['o'])
+
+# 이렇게 가능하지만 선호되지 않는다. 알파벳 순으로 쓰는 것이 룰.
+# import collections, sys
+import collections
+import os
+import sys
+# Leave a space
+# import 3rd party
+print(os.__file__)
+# print(sys.path)
+
+# config= package.config ... 임포트 되는 시점에서 바로 실행된다.
+import package.talk.animal
+print('runner=', __name__)
+
+## 그래서 이런 식으로 안전하게 읽을 수 있도록 한다.
+def main():
+    package.talk.animal.sing()
+    
+if __name__ == '__main__':
+    main()
+    print(__name__)
